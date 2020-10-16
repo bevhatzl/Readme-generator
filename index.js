a7322ea4c70a82ad29ce55d209ca16ac62df0b8d
Bev Hatzl <61686101+bevhatzl@users.noreply.github.com>
2020-10-16 19:32:43 +1100

    basic generator

---
+++ index.js
@@ -0,0 +1,70 @@
+const inquirer = require("inquirer");
+const fs = require("fs");
+const util = require("util");
+const chalk = require("chalk");
+const generateMarkdown = require('./utils/generateMarkdown.js');
+
+const writeFileAsync = util.promisify(fs.writeFile);
+
+// Object constructor function for the user input questions
+function Question(message, name) {
+    this.type = "input";
+    this.message = message;
+    this.name = name;
+}
+
+// Creating instances of the Question object
+let titleQ = new Question("What is the title of your project?", "title");
+let descriptionQ = new Question("Description of your project:", "description");
+let installQ = new Question("What are the installation instructions?", "installInfo");
+let usageQ = new Question("What does the user need to know about running your app?", "usage");
+let contribQ = new Question("What does the user need to know about contributing?", "contrib");
+let testQ = new Question("What are the test instruction?", "test");
+let gitHubUserQ = new Question("What is your GitHub username?", "username");
+let emailQ = new Question("What is your email address?", "email");
+// Create the license question from a list
+let licenseQ = {
+    type: "list",
+    message: "Which license applies to your app?",
+    name: "license",
+    choices: [
+        "Apache License 2.0",
+        "Boost Software License 1.0",
+        "Eclipse Public License 1.0"
+    ]
+}
+
+// array of questions for user
+const questions = [
+    titleQ, descriptionQ, installQ, usageQ, contribQ, testQ, gitHubUserQ, emailQ, licenseQ
+];
+
+
+
+// function to write README file
+function writeToFile(fileName, data) {
+
+}
+
+function promptUser() {
+    return inquirer.prompt(questions);
+}
+
+// function to initialize program
+async function init() {
+    console.log(chalk.green.inverse("Welcome to the good readme generator."));
+    try {
+        const answers = await promptUser();
+
+        const mdGenerated = generateMarkdown(answers);
+
+        await writeFileAsync("./utils/README.md", mdGenerated);
+
+        console.log(chalk.green.inverse("Successfully created a readme file located in the utils folder!"));
+    } catch (err) {
+        console.log(err);
+    }
+}
+
+// function call to initialize program
+init();
---
+++ utils/generateMarkdown.js
@@ -0,0 +1,47 @@
+// To get the correct license icon based on user selection
+function getLicenseIcon(license) {
+    const apache20 = "[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)";
+    const boost = "[![License](https://img.shields.io/badge/License-Boost%201.0-lightblue.svg)](https://www.boost.org/LICENSE_1_0.txt)";
+    const eclipse = "[![License](https://img.shields.io/badge/License-EPL%201.0-red.svg)](https://opensource.org/licenses/EPL-1.0)";
+
+    if (license === "Apache License 2.0") {
+        return apache20;
+    } else if (license === "Boost Software License 1.0") {
+        return boost;
+    } else {
+        return eclipse;
+    }
+}
+
+// function to generate markdown for README
+function generateMarkdown(data) {
+    return `# ${data.title}
+        ${getLicenseIcon(data.license)}
+        ## Description
+        ${data.description}
+        # Table of Contents
+        <ul>
+        [Installation](##installation)
+        [Usage](##usage)
+        [License](##license)
+        [Contributing](##contributing)
+        [Tests](##tests)
+        [Questions](##questions)
+        </ul>
+               
+        ## Installation
+        ${data.installInfo}
+        ## Usage
+        ${data.usage}
+        ## License
+        ${data.license}
+        ## Contributing
+        ${data.contrib}
+        ## Tests
+        ${data.test}
+        ## Questions
+        View my GitHub profile at: <a href="https://github.com/${data.username}">https://github.com/${data.username}/</a>
+        Contact me by email for additional questions with ${data.title} in the subject line: ${data.email}
+  `;
+}
+module.exports = generateMarkdown;
